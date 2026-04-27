e0e7729a4734a971167a29d06bcb455480c42730
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-27 18:09:48 +0700

    menambahkan class belhaketupat dan layang layang dengan akses private dan yang diizinkan

--- tugas.cpp
+++ tugas.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
+class BelahKetupat;
+class LayangLayang
+{
+private:
+    double d1,d2,s1,s2;
+public:
+double hitungLuas(){
+    return 0.5*d1*d2;
+}
+void input(){
+        cout << "Masukkan diagonal Layang-layang : " << endl;
+        cin >> d1;
+        cout << "Masukkan diagonal 2 Layang-layang :  " << endl;
+        cin >> d2;
+        cout << "Masukkan sisi 1 Layang-layang : " << endl;
+        cin >> s1;
+        cout << "Masukkan sisi 2 Layang-layang : " << endl;
+        cin >> s2;
+    }
+    friend void tampilkanKeliling(LayangLayang objLayang, BelahKetupat objBelah);
+};
